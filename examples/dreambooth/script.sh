bc1fb741e2319f1b7f4e5136fc8558598a4ecdd9
Đào Quốc Tuấn <103438257+MrTuanDao@users.noreply.github.com>
2024-04-28 17:09:18 +0700

    Create script.sh

---
+++ examples/dreambooth/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git clone https://huggingface.co/datasets/mrtuandao/nguoideptrainhatthegioi/
+
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export INSTANCE_DIR="nguoideptrainhatthegioi"
+export OUTPUT_DIR="dreambooth-nguoideptrainhatthegioi"
+
+accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --instance_prompt="nguoideptrainhatthegioi" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=400 \
+  --push_to_hub \
+  --use_8bit_adam \
+  --report_to="wandb" \
+  --validation_prompt="a picture of nguoideptrainhatthegioi" \
+  --validation_steps=100
