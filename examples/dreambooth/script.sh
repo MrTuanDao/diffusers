7d1572fd7950a9d62ae664a43425e5a21a806839
Đào Quốc Tuấn <103438257+MrTuanDao@users.noreply.github.com>
2024-04-28 20:21:51 +0700

    Update script.sh

--- examples/dreambooth/script.sh
+++ examples/dreambooth/script.sh
@@ -10,16 +10,16 @@ accelerate launch train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="a photo of nguoideptrainhatthegioi person" \
+  --instance_prompt="a photo of SKStuan person" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
   --learning_rate=5e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=400 \
+  --max_train_steps=1000 \
   --push_to_hub \
   --use_8bit_adam \
   --report_to="wandb" \
-  --validation_prompt="a photo of nguoideptrainhatthegioi person" \
+  --validation_prompt="a photo of SKStuan person" \
   --validation_steps=100
