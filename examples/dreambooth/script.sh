414d81a99a7308b1d78dc98570b3425e43b25b3c
Đào Quốc Tuấn <103438257+MrTuanDao@users.noreply.github.com>
2024-05-01 18:45:33 +0700

    Update script.sh

--- examples/dreambooth/script.sh
+++ examples/dreambooth/script.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-git clone https://huggingface.co/datasets/mrtuandao/nguoideptrainhatthegioi/
-
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
-export INSTANCE_DIR="nguoideptrainhatthegioi"
-export OUTPUT_DIR="dreambooth-nguoideptrainhatthegioi"
+export INSTANCE_DIR="tuan"
+export OUTPUT_DIR="dreambooth-tuan-without-prior"
 
 accelerate launch train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="a photo of SKStuan person" \
+  --instance_prompt="a photo of SKS person" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
   --learning_rate=5e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=1000 \
+  --max_train_steps=4000 \
   --push_to_hub \
-  --use_8bit_adam \
   --report_to="wandb" \
-  --validation_prompt="a photo of SKStuan person" \
+  --validation_prompt="a photo of SKS person" \
   --validation_steps=100
