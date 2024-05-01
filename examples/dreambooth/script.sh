137f22a48fdc60bfc76e791055cc509082c2eeff
Đào Quốc Tuấn <103438257+MrTuanDao@users.noreply.github.com>
2024-05-01 21:56:10 +0700

    Update script.sh

--- examples/dreambooth/script.sh
+++ examples/dreambooth/script.sh
@@ -21,3 +21,30 @@ accelerate launch train_dreambooth.py \
   --validation_prompt="a photo of SKS person" \
   --validation_steps=100 \
   --use_8bit_adam
+
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export INSTANCE_DIR="tuan"
+export CLASS_DIR="person"
+export OUTPUT_DIR="dreambooth-tuan-with-prior"
+
+accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --class_data_dir=$CLASS_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --instance_prompt="a photo of SKS person" \
+  --class_prompt="a photo of person" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=200 \
+  --max_train_steps=800 \
+  --push_to_hub \
+  --report_to="wandb" \
+  --validation_prompt="a photo of SKS person" \
+  --validation_steps=100 \
+  --use_8bit_adam
